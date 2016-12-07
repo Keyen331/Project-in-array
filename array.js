01008fd86741b01ffc357169ee3cfcbc8f1f8885
Keyen331 <banayos.kay@gmail.com>
2016-12-07 08:06:54 -1000

    uploading

---
+++ array.js
@@ -0,0 +1,28 @@
+/*
+
+  Kay Hian Calpito
+  Arrays and Objects
+  Dec, 5, 2016
+
+*/
+
+// Folder name arrays_objects
+// File name arrays.object.js
+//github repo objects_&_arrays
+
+// variables
+// ARRAYS
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var names = ["Senior Tan", "Zakattak", "Yolo Baggins", "Ohai", "Basic Jeff"];
+var colors = ["light black", "ron burgandy", "highlighter"];
+var adj = ["mart", "special case", "yummy", "tiny whiny", "furocious"];
+var action = ["scrap", "kobe beef", "do kine", "li dat", "pau hana"];
+
+
+// OBJECTS
+var donut = {
+  toppings : ["sprinkles", "chocolate frosting", "o'hare air"],
+  fillings : "jelly",
+  shape : "square",
+  count : 12
+}
